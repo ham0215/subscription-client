e07bddd3a3d11053692822172aa97ddf7224645a
Naoto Hamada <naoto.hamada@findy.co.jp>
2022-11-04 12:20:55 +0900

    Add perform

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,23 @@
+import { useState, useMemo, useEffect } from 'react';
+import ActionCable from 'actioncable';
+
 export function Message() {
-  return <div>message</div>;
+  const [message, setMessage] = useState('');
+  const cable = useMemo(() => ActionCable.createConsumer('wss://localhost:3020/cable'), []);
+  const subscription = useMemo(() => {
+    return cable.subscriptions.create("ChatChannel", {
+      received: setMessage
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={() => {
+        subscription.perform('received', { body: 'hoge' })
+      }}>
+        send
+      </button>
+    </div>
+  );
 }
